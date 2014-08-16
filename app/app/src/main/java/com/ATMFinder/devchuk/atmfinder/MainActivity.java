facadddf7d5e446db446f96f84b1fffade71d802
Brian Chuk <brian_chuk@yahoo.com>
2014-08-16 19:02:51 -0400

    more learning

--- app/app/src/main/java/com/ATMFinder/devchuk/atmfinder/MainActivity.java
+++ app/app/src/main/java/com/ATMFinder/devchuk/atmfinder/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -32,5 +33,9 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonOnClick(View v) {
+        // do something when the button is clicked
     }
 }
